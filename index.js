3089f78aee6fb07453a3f6f210658a8afb56763f
khaled-11 <55663179+khaled-11@users.noreply.github.com>
2020-10-28 19:20:20 -0400

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ speech = require("./wit_post_speech"),
 utterances= require("./get_utterances"),
 path = require("path");
 
-utt();
+//utt();
 async function utt(){
   utters = await utterances();
   console.log(utters);
@@ -32,13 +32,20 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.static('public'));
 
 app.get("/", async function (req, res){
-
 nlp = await speech();
 if (nlp.intents && nlp.intents[0]){
-  if (nlp.entities['direction:direction']){
+  if (nlp.intents[0].name === "scroll" && nlp.entities['direction:direction']){
         res.render('index',{intent:`intent: ${nlp.intents[0].name}`, entity: `entity: ${nlp.entities['direction:direction'][0].body}`});
+  } else if (nlp.intents[0].name === "click" && nlp.entities['button_name:button_name']) {
+    res.render('index',{intent:`intent: ${nlp.intents[0].name}`, entity: `entity: ${nlp.entities['button_name:button_name'][0].body}`});
+  } else if (nlp.intents[0].name === "write" && nlp.entities['button_name:button_name']) {
+    res.render('index',{intent:`intent: ${nlp.intents[0].name}`, entity: `entity: ${nlp.entities['field_name:field_name'][0].body}`});
+  } else if (nlp.intents[0].name === "zoom" && nlp.entities['dimension:dimension']) {
+    res.render('index',{intent:`intent: ${nlp.intents[0].name}`, entity: `entity: ${nlp.entities['dimension:dimension'][0].body}`});
+  } else if (nlp.intents[0].name === "refresh") {
+    res.render('index',{intent:`intent: ${nlp.intents[0].name}`, entity: ""});
   } else {
-        res.render('index',{intent:`intent: ${nlp.intents[0].name}`, entity: "What entity?"});
+    res.render('index',{intent:`intent: ${nlp.intents[0].name}`, entity: "What entity?"});
   }
 } else {
     res.render('index',{intent:"No intent!", entity:""});
